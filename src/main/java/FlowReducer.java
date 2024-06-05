b0c10c26bd5e6b48d478d045037ae5b4ac68492a
gdgfhd <xjh19891314912@163.com>
2024-06-05 11:11:41 +0800

    手机流量分析

--- src/main/java/FlowReducer.java
+++ src/main/java/FlowReducer.java
@@ -2,7 +2,6 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
-import java.util.Iterator;
 
 public class FlowReducer extends Reducer<Text, Access, Text, Access> {
 
